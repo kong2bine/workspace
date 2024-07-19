8151c1f7cd10918d7cd4534f4a81c29fbdf02e26
kong2bine <kong2bine@naver.com>
2024-07-19 16:38:51 +0900

    1

---
+++ workspace_spring/ReactProduct/src/main/java/com/green/ReactProduct/ReactProductApplication.java
@@ -0,0 +1,13 @@
+package com.green.ReactProduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactProductApplication.class, args);
+	}
+
+}
---
+++ workspace_spring/ReactProduct/src/test/java/com/green/ReactProduct/ReactProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.ReactProduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
